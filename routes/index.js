06136f51563c35df3fcf05517fc0174971743aeb
Jess <iam@jesszh.com>
2016-08-06 23:38:10 +0800

    adjust some

--- routes/index.js
+++ routes/index.js
@@ -26,22 +26,22 @@ router.post('/login', passport.authenticate('local-login', {
 }));
 
 
-// =====================================
-// SIGNUP ==============================
-// =====================================
-// show the signup form
-router.get('/signup', function(req, res) {
+// // =====================================
+// // SIGNUP ==============================
+// // =====================================
+// // show the signup form
+// router.get('/signup', function(req, res) {
 
-    // render the page and pass in any flash data if it exists
-    res.render('signup', { message: req.flash('signupMessage') });
-});
+//     // render the page and pass in any flash data if it exists
+//     res.render('signup', { message: req.flash('signupMessage') });
+// });
 
-// process the signup form
-router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect : '/', // redirect to the secure profile section
-    failureRedirect : 'signup', // redirect back to the signup page if there is an error
-    failureFlash : true // allow flash messages
-}));
+// // process the signup form
+// router.post('/signup', passport.authenticate('local-signup', {
+//     successRedirect : '/', // redirect to the secure profile section
+//     failureRedirect : 'signup', // redirect back to the signup page if there is an error
+//     failureFlash : true // allow flash messages
+// }));
 
 // =====================================
 // LOGOUT ==============================
